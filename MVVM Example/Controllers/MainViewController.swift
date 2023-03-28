5c021c1318f41085bcffa87c2016bc66d40878a1
Mikail <mikailbaykaraa@gmail.com>
2023-03-28 15:38:24 +0300

    installed alamofire and prepared mvvm architecture

---
+++ MVVM Example/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MVVM Example
+//
+//  Created by Mikail Baykara on 25.03.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ MVVM Example/ViewModel/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  MVVM Example
+//
+//  Created by Mikail Baykara on 20.03.2023.
+//
+
+import Foundation
+
+final class LoginViewModel{
+    
+    
+}
